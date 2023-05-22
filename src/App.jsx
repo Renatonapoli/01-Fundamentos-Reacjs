9ea58b349f72d38bf14a229db909a4820a11fac3
Renato Napoli <renato.napoli@nagro.app>
2023-05-22 17:51:56 -0300

    Fix: Estrutura e componentes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from "./post"
+
+export function App() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
