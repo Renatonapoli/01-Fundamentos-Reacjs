085850a0df060f9f3e23cfaacacb1c0fe7ebba17
Renato Napoli <renato.napoli@nagro.app>
2023-05-23 17:13:55 -0300

    Feat: Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { Post } from "./post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Renato Napoli"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio facilis reiciendis itaque ipsum delectus iste, qui aliquam, minus eligendi vitae repudiandae, atque error expedita molestiae quia quam facere eaque recusandae."
+      />
+      <Post author="Grabriel Buzzi" content="Novo post muito legal" />
     </div>
   )
 }
