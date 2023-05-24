83b879182850e69f3bc7b484d6980a00fbad33e6
Renato Napoli <renato.napoli@nagro.app>
2023-05-24 20:51:14 -0300

    Feat: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./post"
+
+import styles from "./App.module.css"
 
 import "../src/global.css"
 
@@ -8,11 +11,16 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Renato Napoli"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio facilis reiciendis itaque ipsum delectus iste, qui aliquam, minus eligendi vitae repudiandae, atque error expedita molestiae quia quam facere eaque recusandae."
-      />
-      <Post author="Grabriel Buzzi" content="Novo post muito legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Renato Napoli"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio facilis reiciendis itaque ipsum delectus iste, qui aliquam, minus eligendi vitae repudiandae, atque error expedita molestiae quia quam facere eaque recusandae."
+          />
+          <Post author="Grabriel Buzzi" content="Novo post muito legal" />
+        </main>
+      </div>
     </div>
   )
 }
