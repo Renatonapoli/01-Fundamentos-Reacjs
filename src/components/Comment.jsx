d8ef75ac927860fc005c19cfc01ef3f5f6666c6f
Renato Napoli <renato.napoli@nagro.app>
2023-05-26 15:50:35 -0300

    Feat: Estilizacoes do comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,27 +11,26 @@ export function Comment() {
           <header>
             <div className={styles.authorAndTime}>
               <strong>Renato Napoli</strong>
-
               <time title="11 de Maio ás 08:13h" dateTime="2022-05-11 08:13:30">
                 Cerca de 1h atrás
               </time>
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
           <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-      </div>
 
-      <footer>
-        <button>
-          <ThumbsUp />
-        </button>
-        Aplaudir <span>20</span>
-      </footer>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
     </div>
   )
 }
