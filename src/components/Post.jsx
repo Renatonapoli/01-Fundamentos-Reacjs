8f056bc5166afea26ba8b1cc6b664988ab60e818
Renato Napoli <renato.napoli@nagro.app>
2023-05-29 17:40:19 -0300

    Feat: Removendo comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    const commentWidthoutDeleteOne = comments.filter((comment) => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentWidthoutDeleteOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -83,7 +91,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
